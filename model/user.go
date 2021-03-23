727a5db5aa2ac3ee5d230a6143f340c8f8dfd58d
José Pereira <hetfdex@gmail.com>
2021-03-23 16:44:42 +0000

    Added password and hash props

--- model/user.go
+++ model/user.go
@@ -2,6 +2,8 @@ package model
 
 type User struct {
 	Base
-	Name  string
-	Email string `gorm:"not null;unique_index"`
+	Name         string
+	Email        string `gorm:"not null;unique_index"`
+	Password     string `gorm:"-"`
+	PasswordHash string `gorm:"not null"`
 }
