49291e05beccd4a664f5a5ad9af12e7795c2b087
José Pereira <hetfdex@gmail.com>
2021-03-23 16:10:55 +0000

    Refactored to use user service

--- main.go
+++ main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"lens-locked-go/config"
 	"lens-locked-go/controller"
+	"lens-locked-go/service"
 	"net/http"
 )
 
 func main() {
+	us, err := service.New(config.Dsn)
+
+	if err != nil {
+		panic(err)
+	}
+	_ = us.DropTable()
+	_ = us.CreateTable()
+
 	homeController := controller.NewHomeController()
-	registerController := controller.NewRegisterController()
+	registerController := controller.NewRegisterController(us)
 
 	r := mux.NewRouter()
 
@@ -16,7 +26,7 @@ func main() {
 	r.HandleFunc(registerController.Route, registerController.Handle).Methods(http.MethodGet)
 	r.HandleFunc(registerController.Route, registerController.Register).Methods(http.MethodPost)
 
-	err := http.ListenAndServe("localhost:8080", r)
+	err = http.ListenAndServe("localhost:8080", r)
 
 	if err != nil {
 		panic(err)
