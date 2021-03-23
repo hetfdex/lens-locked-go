3debd02a52a824783ada5e8658af56adaac81563
José Pereira <hetfdex@gmail.com>
2021-03-23 16:10:29 +0000

    Connected to user service and called create user

--- controller/register.go
+++ controller/register.go
@@ -3,16 +3,19 @@ package controller
 import (
 	"fmt"
 	"lens-locked-go/model"
+	"lens-locked-go/service"
 	"net/http"
 )
 
 type registerController struct {
 	*controller
+	*service.UserService
 }
 
-func NewRegisterController() *registerController {
+func NewRegisterController(us *service.UserService) *registerController {
 	return &registerController{
 		newController("/register", "view/register.gohtml"),
+		us,
 	}
 }
 
@@ -22,7 +25,19 @@ func (c *registerController) Register(w http.ResponseWriter, req *http.Request) 
 	err := parseForm(req, registration)
 
 	if err != nil {
-		panic(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+
+		return
 	}
-	_, _ = fmt.Fprintln(w, *registration)
+	user := &model.User{
+		Name:  "",
+		Email: registration.Email,
+	}
+
+	err = c.Create(user)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	_, _ = fmt.Fprintln(w, user.Email)
 }
