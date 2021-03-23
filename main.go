2a72bf5d025603b47cf9b76d14c28c8c4c0fce7d
José Pereira <hetfdex@gmail.com>
2021-03-23 14:55:16 +0000

    Created db tests

--- main.go
+++ main.go
@@ -1,12 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
+	"lens-locked-go/config"
 	"lens-locked-go/controller"
+	"lens-locked-go/model"
+	"lens-locked-go/service"
 	"net/http"
 )
 
 func main() {
+	us, err := service.New(config.Dsn)
+
+	if err != nil {
+		panic(err)
+	}
+	err = us.DropTable()
+
+	if err != nil {
+		panic(err)
+	}
+	err = us.CreateTable()
+
+	if err != nil {
+		panic(err)
+	}
+
+	u := &model.User{
+		Name:  "Jose",
+		Email: "test@email.com",
+	}
+
+	err = us.Create(u)
+
+	if err != nil {
+		panic(err)
+	}
+	result, err := us.ById(u.ID)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result.ID, result.Name, result.Email)
+
+	u.Email = "fake@email.com"
+
+	err = us.Update(u)
+
+	result, err = us.ByEmail(u.Email)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result.ID, result.Name, result.Email)
+
 	homeController := controller.NewHomeController()
 	registerController := controller.NewRegisterController()
 
@@ -16,7 +64,7 @@ func main() {
 	r.HandleFunc(registerController.Route, registerController.Handle).Methods(http.MethodGet)
 	r.HandleFunc(registerController.Route, registerController.Register).Methods(http.MethodPost)
 
-	err := http.ListenAndServe("localhost:8080", r)
+	err = http.ListenAndServe("localhost:8080", r)
 
 	if err != nil {
 		panic(err)
