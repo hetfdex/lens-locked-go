5141b463fc4139de3097eee7f8eb7f9d14bff179
José Pereira <hetfdex@gmail.com>
2021-03-24 15:32:19 +0000

    Refactored controller

--- controller/controller.go
+++ controller/controller.go
@@ -10,17 +10,17 @@ type controller struct {
 	view  *view.View
 }
 
-func newController(route string, filename string) *controller {
-	return &controller{
-		Route: route,
-		view:  view.New(filename),
-	}
-}
-
 func (c *controller) Handle(w http.ResponseWriter, _ *http.Request) {
 	err := c.view.Render(w, nil)
 
 	if err != nil {
 		http.Error(w, err.Message, err.StatusCode)
+	}
+}
+
+func newController(route string, filename string) *controller {
+	return &controller{
+		Route: route,
+		view:  view.New(filename),
 	}
 }
