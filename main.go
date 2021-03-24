947cf9a13cd9f7ec3a9099824dec4dc0d48d8cea
José Pereira <hetfdex@gmail.com>
2021-03-24 16:50:01 +0000

    Refactored code

--- main.go
+++ main.go
@@ -28,15 +28,15 @@ func main() {
 }
 
 func configureRouter(us *service.UserService, r *mux.Router) {
-	homeController := controller.NewHomeController()
+	homeController := controller.NewHomeController(us)
 	registerController := controller.NewRegisterController(us)
 	loginController := controller.NewLoginController(us)
 
-	r.HandleFunc(homeController.Route, homeController.Handle).Methods(http.MethodGet)
-	r.HandleFunc(registerController.Route, registerController.Handle).Methods(http.MethodGet)
-	r.HandleFunc(registerController.Route, registerController.Register).Methods(http.MethodPost)
-	r.HandleFunc(loginController.Route, loginController.Handle).Methods(http.MethodGet)
-	r.HandleFunc(loginController.Route, loginController.Login).Methods(http.MethodPost)
+	r.HandleFunc(homeController.Route, homeController.Get).Methods(http.MethodGet)
+	r.HandleFunc(registerController.Route, registerController.Get).Methods(http.MethodGet)
+	r.HandleFunc(registerController.Route, registerController.Post).Methods(http.MethodPost)
+	r.HandleFunc(loginController.Route, loginController.Get).Methods(http.MethodGet)
+	r.HandleFunc(loginController.Route, loginController.Post).Methods(http.MethodPost)
 }
 
 func cleanDatabase(us *service.UserService) {
