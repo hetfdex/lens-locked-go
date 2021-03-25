109f2050bec93bcd6d9cd4d4ff06759107a62575
José Pereira <hetfdex@gmail.com>
2021-03-25 16:18:44 +0000

    Implemented edit user and refactored code to return token directly

--- service/user.go
+++ service/user.go
@@ -8,8 +8,9 @@ import (
 )
 
 type IUserService interface {
-	Register(register *model.RegisterForm) (*model.User, *model.ApiError)
-	LoginWithPassword(login *model.LoginForm) (*model.User, *model.ApiError)
+	Register(register *model.RegisterForm) (*model.User, string, *model.ApiError)
+	Edit(update *model.UpdateForm, token string) (*model.User, string, *model.ApiError)
+	LoginWithPassword(login *model.LoginForm) (*model.User, string, *model.ApiError)
 	LoginWithToken(token string) (*model.User, *model.ApiError)
 }
 
@@ -30,89 +31,132 @@ func NewUserService(ur repository.IUserRepository) *userService {
 	}
 }
 
-func (us *userService) Register(register *model.RegisterForm) (*model.User, *model.ApiError) {
+func (us *userService) Register(register *model.RegisterForm) (*model.User, string, *model.ApiError) {
 	register.Email = normalizeEmail(register.Email)
 
 	if !validEmail(register.Email) {
-		return nil, model.NewBadRequestApiError("invalid email")
+		return nil, "", model.NewBadRequestApiError("invalid email")
 	}
 	user, _ := us.getByEmail(register.Email)
 
 	if user != nil {
-		return nil, model.NewConflictApiError("user already exists")
+		return nil, "", model.NewConflictApiError("user already exists")
 	}
 
 	if !validPassword(register.Password) {
-		return nil, model.NewBadRequestApiError("invalid password")
+		return nil, "", model.NewBadRequestApiError("invalid password")
 	}
 	pwHash, err := generateFromPassword(register.Password)
 
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
-
 	token, err := generateToken()
 
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
-
 	tokenHash, err := us.Hasher.GenerateTokenHash(token)
 
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
-
-	user = model.NewUserFromRegister(register, pwHash, token, tokenHash)
+	user = model.NewUserFromRegister(register, pwHash, tokenHash)
 
 	err = us.Create(user)
 
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
-	return user, nil
+	return user, token, nil
 }
 
-func (us *userService) LoginWithPassword(login *model.LoginForm) (*model.User, *model.ApiError) {
+func (us *userService) Edit(update *model.UpdateForm, token string) (*model.User, string, *model.ApiError) {
+	update.Email = normalizeEmail(update.Email)
+
+	if !validEmail(update.Email) {
+		return nil, "", model.NewBadRequestApiError("invalid email")
+	}
+
+	if !validPassword(update.Password) {
+		return nil, "", model.NewBadRequestApiError("invalid password")
+	}
+	loggedInUser, err := us.LoginWithToken(token)
+
+	if err != nil {
+		return nil, "", err
+	}
+	userFromEmail, _ := us.getByEmail(update.Email)
+
+	if userFromEmail != nil {
+		if loggedInUser.DeepEqual(userFromEmail) {
+			return nil, "", model.NewBadRequestApiError("users are the same")
+		}
+		return nil, "", model.NewConflictApiError("user already exists")
+	}
+	pwHash, err := generateFromPassword(update.Password)
+
+	if err != nil {
+		return nil, "", err
+	}
+	newToken, err := generateToken()
+
+	if err != nil {
+		return nil, "", err
+	}
+	newTokenHash, err := us.Hasher.GenerateTokenHash(newToken)
+
+	if err != nil {
+		return nil, "", err
+	}
+	updatedUser := model.NewUserFromUpdate(update, pwHash, newTokenHash)
+
+	err = us.Update(updatedUser)
+
+	if err != nil {
+		return nil, "", err
+	}
+	return updatedUser, newToken, nil
+}
+
+func (us *userService) LoginWithPassword(login *model.LoginForm) (*model.User, string, *model.ApiError) {
 	login.Email = normalizeEmail(login.Email)
 
 	if !validEmail(login.Email) {
-		return nil, model.NewBadRequestApiError("invalid email")
+		return nil, "", model.NewBadRequestApiError("invalid email")
 	}
 	user, err := us.getByEmail(login.Email)
 
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
 
 	if !validPassword(login.Password) {
-		return nil, model.NewBadRequestApiError("invalid password")
+		return nil, "", model.NewBadRequestApiError("invalid password")
 	}
 	err = compareHashAndPassword(user.PasswordHash, login.Password)
 
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
 	token, err := generateToken()
 
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
-	user.Token = token
-
 	tokenHash, err := us.Hasher.GenerateTokenHash(token)
 
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
 	user.TokenHash = tokenHash
 
 	err = us.Update(user)
 
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
-	return user, nil
+	return user, token, nil
 }
 
 func (us *userService) LoginWithToken(token string) (*model.User, *model.ApiError) {
