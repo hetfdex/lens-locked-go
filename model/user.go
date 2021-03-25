6098589239b95ae5f75c408d1f9fecfb5573f829
José Pereira <hetfdex@gmail.com>
2021-03-25 16:17:37 +0000

    Implemented new user from update and deep equal

--- model/user.go
+++ model/user.go
@@ -5,16 +5,50 @@ type User struct {
 	Name         string
 	Email        string `gorm:"not null;unique_index"`
 	PasswordHash string `gorm:"not null"`
-	Token        string `gorm:"-"`
 	TokenHash    string `gorm:"not null;unique_index"`
 }
 
-func NewUserFromRegister(register *RegisterForm, passwordHash string, token string, tokenHash string) *User {
+func NewUserFromRegister(register *RegisterForm, passwordHash string, tokenHash string) *User {
 	return &User{
 		Name:         register.Name,
 		Email:        register.Email,
 		PasswordHash: passwordHash,
-		Token:        token,
 		TokenHash:    tokenHash,
 	}
+}
+
+func NewUserFromUpdate(update *UpdateForm, passwordHash string, tokenHash string) *User {
+	return &User{
+		Name:         update.Name,
+		Email:        update.Email,
+		PasswordHash: passwordHash,
+		TokenHash:    tokenHash,
+	}
+}
+
+func (u *User) DeepEqual(other *User) bool {
+	if u.ID != other.ID {
+		return false
+	}
+
+	if u.CreatedAt != other.CreatedAt {
+		return false
+	}
+
+	if u.UpdatedAt != other.UpdatedAt {
+		return false
+	}
+
+	if u.DeletedAt != other.DeletedAt {
+		return false
+	}
+
+	if u.Name != other.Name {
+		return false
+	}
+
+	if u.Email != other.Email {
+		return false
+	}
+	return true
 }
