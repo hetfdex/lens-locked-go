c9f42feac2532554a140ac95f90e82707d83d1b5
José Pereira <hetfdex@gmail.com>
2021-03-26 16:18:40 +0000

    Implemented new gallery

--- service/gallery.go
+++ service/gallery.go
@@ -1,10 +1,14 @@
 package service
 
 import (
+	"lens-locked-go/model"
 	"lens-locked-go/repository"
 )
 
+const titleInUseErrorMessage = "title is already in use"
+
 type IGalleryService interface {
+	New(*model.NewGallery) (*model.Gallery, *model.Error)
 }
 
 type galleryService struct {
@@ -15,4 +19,34 @@ func NewGalleryService(ur repository.IGalleryRepository) *galleryService {
 	return &galleryService{
 		ur,
 	}
+}
+
+func (s *galleryService) New(create *model.NewGallery) (*model.Gallery, *model.Error) {
+	create.Title = normalizeEmail(create.Title)
+
+	gallery, _ := s.getByTitle(create.Title)
+
+	if gallery != nil && gallery.UserId.String() == "TODO" {
+		return nil, model.NewConflictApiError(titleInUseErrorMessage)
+	}
+	gallery = create.Gallery()
+
+	err := s.Create(gallery)
+
+	if err != nil {
+		return nil, err
+	}
+	return gallery, nil
+}
+
+func (s *galleryService) getByTitle(title string) (*model.Gallery, *model.Error) {
+	if title == "" {
+		return nil, model.NewInternalServerApiError(model.MustNotBeEmptyErrorMessage("title"))
+	}
+	gallery, err := s.Read("title", title)
+
+	if err != nil {
+		return nil, err
+	}
+	return gallery, nil
 }
