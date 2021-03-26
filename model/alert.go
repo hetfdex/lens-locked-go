3155a9ef0a846f34e4599d2df50152c7f64a4a0b
José Pereira <hetfdex@gmail.com>
2021-03-26 11:33:24 +0000

    Removed unused code

--- model/alert.go
+++ model/alert.go
@@ -2,16 +2,10 @@ package model
 
 const alertLevelError = "danger"
 const alertLevelWarning = "warning"
-const alertLevelInfo = "info"
-const alertLevelSuccess = "success"
 
 type Alert struct {
 	Level   string
 	Message string
-}
-
-func NewSuccessAlert(message string) *Alert {
-	return newAlert(alertLevelSuccess, message)
 }
 
 func newAlert(level string, message string) *Alert {
