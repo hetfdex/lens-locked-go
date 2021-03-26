efeb1722559e4ef0e8f7cba1af7d85c72e70d9e5
José Pereira <hetfdex@gmail.com>
2021-03-26 10:36:39 +0000

    Removed constructors

--- model/alert.go
+++ model/alert.go
@@ -10,22 +10,6 @@ type Alert struct {
 	Message string
 }
 
-func NewErrorAlert(message string) *Alert {
-	return newAlert(AlertLevelError, message)
-}
-
-func NewWarningAlert(message string) *Alert {
-	return newAlert(AlertLevelWarning, message)
-}
-
-func NewInfoAlert(message string) *Alert {
-	return newAlert(AlertLevelInfo, message)
-}
-
-func NewSuccessAlert(message string) *Alert {
-	return newAlert(AlertLevelSuccess, message)
-}
-
 func newAlert(level string, message string) *Alert {
 	return &Alert{
 		Level:   level,
