d80e87a364c7f4710d1fc616b9f6b9aaaef6a810
José Pereira <hetfdex@gmail.com>
2021-03-26 10:21:31 +0000

    Added constructors for different alert levels

--- model/alert.go
+++ model/alert.go
@@ -9,3 +9,26 @@ type Alert struct {
 	Level   string
 	Message string
 }
+
+func NewErrorAlert(message string) *Alert {
+	return newAlert(AlertLevelError, message)
+}
+
+func NewWarningAlert(message string) *Alert {
+	return newAlert(AlertLevelWarning, message)
+}
+
+func NewInfoAlert(message string) *Alert {
+	return newAlert(AlertLevelInfo, message)
+}
+
+func NewSuccessAlert(message string) *Alert {
+	return newAlert(AlertLevelSuccess, message)
+}
+
+func newAlert(level string, message string) *Alert {
+	return &Alert{
+		Level:   level,
+		Message: message,
+	}
+}
