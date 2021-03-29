750cf67bf2858c94e8d6bcbc06e46251498e7abb
José Pereira <hetfdex@gmail.com>
2021-03-29 13:07:28 +0100

    Implemented make url and get gallery

--- controller/util.go
+++ controller/util.go
@@ -1,8 +1,11 @@
 package controller
 
 import (
+	"github.com/gofrs/uuid"
+	"github.com/gorilla/mux"
 	"github.com/gorilla/schema"
 	"lens-locked-go/model"
+	"lens-locked-go/service"
 	"lens-locked-go/view"
 	"net/http"
 )
@@ -18,6 +21,9 @@ const registerUserFilename = "view/user_register.gohtml"
 
 const createGalleryRoute = "/gallery/create"
 const createGalleryFilename = "view/gallery_create.gohtml"
+
+const editGalleryRoute = "/gallery/{id}/edit"
+const editGalleryFilename = "view/gallery_edit.gohtml"
 
 const GalleryRouteName = "gallery"
 const galleryRoute = "/gallery/{id}"
@@ -65,4 +71,33 @@ func handleError(view *view.View, w http.ResponseWriter, err *model.Error, data 
 	w.WriteHeader(err.StatusCode)
 
 	view.Render(w, data)
+}
+
+func makeUrl(router *mux.Router, routeName string, key string, value string) (string, *model.Error) {
+	url, err := router.Get(routeName).URL(key, value)
+
+	if err != nil {
+		er := model.NewInternalServerApiError(err.Error())
+
+		return "", er
+	}
+	return url.String(), nil
+}
+
+func getGallery(req *http.Request, gs service.IGalleryService) (*model.Gallery, *model.Error) {
+	vars := mux.Vars(req)
+
+	id := uuid.FromStringOrNil(vars[idKey])
+
+	if id == uuid.Nil {
+		err := model.NewBadRequestApiError(invalidUUIDErrorMessage)
+
+		return nil, err
+	}
+	gallery, err := gs.Get(id)
+
+	if err != nil {
+		return nil, err
+	}
+	return gallery, nil
 }
