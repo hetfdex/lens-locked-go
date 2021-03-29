6d959688aaaeb7d49e43cbca8eae08266d4b1577
José Pereira <hetfdex@gmail.com>
2021-03-29 13:45:31 +0100

    Refactored home controller

--- controller/home.go
+++ controller/home.go
@@ -1,44 +1,30 @@
 package controller
 
 import (
-	"errors"
 	"lens-locked-go/model"
-	"lens-locked-go/service"
 	"lens-locked-go/view"
 	"net/http"
 )
 
+const homeRoute = "/"
+const homeFilename = "view/home.gohtml"
+
 type homeController struct {
-	Route       string
-	view        *view.View
-	userService service.IUserService
+	homeView *view.View
 }
 
-func NewHomeController(us service.IUserService) *homeController {
-	return newHomeController(homeRoute, homeFilename, us)
+func NewHomeController() *homeController {
+	return &homeController{
+		homeView: view.New(homeRoute, homeFilename),
+	}
 }
 
-func (c *homeController) Get(w http.ResponseWriter, _ *http.Request) {
+func (c *homeController) HomeGet(w http.ResponseWriter, _ *http.Request) {
 	data := &model.DataView{}
 
-	c.view.Render(w, data)
+	c.homeView.Render(w, data)
 }
 
-func (c *homeController) handleError(w http.ResponseWriter, err *model.Error, data *model.DataView) {
-	data.Alert = err.Alert()
-
-	w.WriteHeader(err.StatusCode)
-
-	c.view.Render(w, data)
-}
-
-func newHomeController(route string, filename string, us service.IUserService) *homeController {
-	if route == "" {
-		panic(errors.New(model.MustNotBeEmptyErrorMessage("route")))
-	}
-	return &homeController{
-		Route:       route,
-		view:        view.New(filename),
-		userService: us,
-	}
+func (c *homeController) HomeRoute() string {
+	return c.homeView.Route()
 }
