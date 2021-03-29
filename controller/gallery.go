7412ce9b34cc154a5962876fd470576f646c6cd4
José Pereira <hetfdex@gmail.com>
2021-03-29 12:14:17 +0100

    Implemented get gallery

--- controller/gallery.go
+++ controller/gallery.go
@@ -2,11 +2,17 @@ package controller
 
 import (
 	"errors"
+	"github.com/gofrs/uuid"
+	"github.com/gorilla/mux"
 	"lens-locked-go/model"
 	"lens-locked-go/service"
 	"lens-locked-go/view"
 	"net/http"
 )
+
+const idKey = "id"
+
+const invalidUUIDErrorMessage = "invalid gallery id"
 
 type galleryController struct {
 	Route          string
@@ -18,8 +24,28 @@ func NewGalleryController(gs service.IGalleryService) *galleryController {
 	return newGalleryController(galleryRoute, galleryFilename, gs)
 }
 
-func (c *galleryController) Get(w http.ResponseWriter, _ *http.Request) {
+func (c *galleryController) Get(w http.ResponseWriter, req *http.Request) {
 	data := &model.DataView{}
+
+	vars := mux.Vars(req)
+
+	id := uuid.FromStringOrNil(vars[idKey])
+
+	if id == uuid.Nil {
+		err := model.NewBadRequestApiError(invalidUUIDErrorMessage)
+
+		handleError(c.view, w, err, data)
+
+		return
+	}
+	gallery, err := c.galleryService.Get(id)
+
+	if err != nil {
+		handleError(c.view, w, err, data)
+
+		return
+	}
+	data.Data = gallery
 
 	c.view.Render(w, data)
 }
