0c4faf726cddf5e6f9198e965fba7e0ae9414ad0
José Pereira <hetfdex@gmail.com>
2021-03-29 14:36:29 +0100

    Refactored user

--- context/context.go
+++ context/context.go
@@ -6,18 +6,19 @@ import (
 )
 
 const userKey = "user"
+const noUserInContextErrorMessage = "no user in context"
 
 func WithUser(ctx context.Context, user *model.User) context.Context {
 	return context.WithValue(ctx, userKey, user)
 }
 
-func User(ctx context.Context) *model.User {
+func User(ctx context.Context) (*model.User, *model.Error) {
 	value := ctx.Value(userKey)
 
 	if value != nil {
 		if user, ok := value.(*model.User); ok {
-			return user
+			return user, nil
 		}
 	}
-	return nil
+	return nil, model.NewInternalServerApiError(noUserInContextErrorMessage)
 }
