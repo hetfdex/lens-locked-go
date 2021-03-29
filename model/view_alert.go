c915f461570b7e60cf01f0dd894fa5d35a2c98f8
José Pereira <hetfdex@gmail.com>
2021-03-29 16:51:26 +0100

    Implemented success constructor

--- model/view_alert.go
+++ model/view_alert.go
@@ -2,8 +2,16 @@ package model
 
 const alertLevelError = "danger"
 const alertLevelWarning = "warning"
+const alertLevelSuccess = "success"
 
 type AlertView struct {
 	Level   string
 	Message string
+}
+
+func NewSuccessAlert(message string) *AlertView {
+	return &AlertView{
+		Level:   alertLevelSuccess,
+		Message: message,
+	}
 }
