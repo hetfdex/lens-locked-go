90dafe043d503a9ad07a6ded01a4a89a065727ef
José Pereira <hetfdex@gmail.com>
2021-03-30 17:45:38 +0100

    Created image model

---
+++ model/image.go
@@ -0,0 +1,10 @@
+package model
+
+import "github.com/gofrs/uuid"
+
+type Image struct {
+	Base
+	Bytes     []byte    `gorm:"not null"`
+	Extension string    `gorm:"not null"`
+	GalleryId uuid.UUID `gorm:"not null;index"`
+}
