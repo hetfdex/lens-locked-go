60fa0f438287cb3d92f0af9d060ef5be7e7558f2
José Pereira <hetfdex@gmail.com>
2021-03-30 17:35:16 +0100

    Refactored services

--- service/user.go
+++ service/user.go
@@ -1,15 +1,25 @@
 package service
 
 import (
+	"golang.org/x/crypto/bcrypt"
 	"lens-locked-go/hash"
 	"lens-locked-go/model"
+	"lens-locked-go/rand"
 	"lens-locked-go/repository"
+	"regexp"
+	"strings"
 )
 
+const pepper = "6Sk65RHhGW7S4qnVPV7m"
+
 const key = "yzzmGPkAA9FTmbtzz9jB"
+
 const invalidEmailErrorMessage = "invalid email address"
+const invalidPasswordErrorMessage = "invalid password"
 const emailInUseErrorMessage = "email address is already in use"
 const invalidPasswordLengthErrorMessage = "password must be at least 8 characters"
+
+var emailRegex = regexp.MustCompile(`^[a-z0-9_.+-]+@[a-z0-9-]+\.[a-z0-9-.]+$`)
 
 type IUserService interface {
 	Register(*model.RegisterUser) (*model.User, string, *model.Error)
@@ -167,4 +177,68 @@ func (s *userService) getByTokenHash(hash string) (*model.User, *model.Error) {
 		return nil, err
 	}
 	return user, nil
+}
+
+func generateHashFromPassword(password string) (string, *model.Error) {
+	if password == "" {
+		return "", model.NewInternalServerApiError(model.MustNotBeEmptyErrorMessage("password"))
+	}
+
+	hs, err := bcrypt.GenerateFromPassword([]byte(password+pepper), bcrypt.DefaultCost)
+
+	if err != nil {
+		return "", model.NewInternalServerApiError(err.Error())
+	}
+	return string(hs), nil
+}
+
+func compareHashAndPassword(hash string, password string) *model.Error {
+
+	if hash == "" {
+		return model.NewInternalServerApiError(model.MustNotBeEmptyErrorMessage("hash"))
+	}
+
+	if password == "" {
+		return model.NewInternalServerApiError(model.MustNotBeEmptyErrorMessage("password"))
+	}
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password+pepper))
+
+	if err != nil {
+		if err == bcrypt.ErrMismatchedHashAndPassword {
+			return model.NewForbiddenApiError(invalidPasswordErrorMessage)
+		}
+		return model.NewInternalServerApiError(err.Error())
+	}
+	return nil
+}
+
+func generateToken() (string, *model.Error) {
+	token, err := rand.GenerateTokenString()
+
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
+
+func lower(s string) string {
+	return strings.ToLower(s)
+}
+
+func validEmail(email string) bool {
+	if len(email) < 3 && len(email) > 254 {
+		return false
+	}
+
+	if !emailRegex.MatchString(email) {
+		return false
+	}
+	return true
+}
+
+func validPassword(password string) bool {
+	if len(password) < 8 {
+		return false
+	}
+	return true
 }
--- service/util.go
+++ service/util.go
@@ -1,82 +1,9 @@
 package service
 
 import (
-	"golang.org/x/crypto/bcrypt"
-	"lens-locked-go/model"
-	"lens-locked-go/rand"
-	"regexp"
 	"strings"
 )
 
-const pepper = "6Sk65RHhGW7S4qnVPV7m"
-const invalidPasswordErrorMessage = "invalid password"
-
-var emailRegex = regexp.MustCompile(`^[a-z0-9_.+-]+@[a-z0-9-]+\.[a-z0-9-.]+$`)
-
-func generateHashFromPassword(password string) (string, *model.Error) {
-	if password == "" {
-		return "", model.NewInternalServerApiError(model.MustNotBeEmptyErrorMessage("password"))
-	}
-
-	hash, err := bcrypt.GenerateFromPassword([]byte(password+pepper), bcrypt.DefaultCost)
-
-	if err != nil {
-		return "", model.NewInternalServerApiError(err.Error())
-	}
-	return string(hash), nil
-}
-
-func compareHashAndPassword(hash string, password string) *model.Error {
-
-	if hash == "" {
-		return model.NewInternalServerApiError(model.MustNotBeEmptyErrorMessage("hash"))
-	}
-
-	if password == "" {
-		return model.NewInternalServerApiError(model.MustNotBeEmptyErrorMessage("password"))
-	}
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password+pepper))
-
-	if err != nil {
-		if err == bcrypt.ErrMismatchedHashAndPassword {
-			return model.NewForbiddenApiError(invalidPasswordErrorMessage)
-		}
-		return model.NewInternalServerApiError(err.Error())
-	}
-	return nil
-}
-
-func generateToken() (string, *model.Error) {
-	token, err := rand.GenerateTokenString()
-
-	if err != nil {
-		return "", err
-	}
-	return token, nil
-}
-
-func lower(s string) string {
-	return strings.ToLower(s)
-}
-
 func trimSpace(s string) string {
 	return strings.TrimSpace(s)
-}
-
-func validEmail(email string) bool {
-	if len(email) < 3 && len(email) > 254 {
-		return false
-	}
-
-	if !emailRegex.MatchString(email) {
-		return false
-	}
-	return true
-}
-
-func validPassword(password string) bool {
-	if len(password) < 8 {
-		return false
-	}
-	return true
 }
