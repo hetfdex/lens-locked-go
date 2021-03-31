24f63911d6ad841eaa24d6b458f74f474451eb6c
José Pereira <hetfdex@gmail.com>
2021-03-31 14:18:36 +0100

    Changed model to take string

--- model/image.go
+++ model/image.go
@@ -4,7 +4,7 @@ import "github.com/gofrs/uuid"
 
 type Image struct {
 	Base
-	Bytes     []byte    `gorm:"not null"`
+	Source    string    `gorm:"not null"`
 	Name      string    `gorm:"not null"`
 	Extension string    `gorm:"not null"`
 	GalleryId uuid.UUID `gorm:"not null;index"`
